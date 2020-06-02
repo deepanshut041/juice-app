c6c0d416c0d6fa6180c1c7f8f99e520e529c2ce5
Deepanshu Tyagi <deepanshut041@gmail.com>
2020-06-02 16:31:19 +0530

    Added splash to application while token processing

--- App.tsx
+++ App.tsx
@@ -1,26 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import SignInScreen from './components/auth/SignInScreen';
 import SignUpScreen from './components/auth/SignUpScreen';
-import  HomeScreen  from "./components/HomeScreen";
-import  AboutScreen  from "./components/AboutScreen";
+import HomeScreen from "./components/HomeScreen";
+import AboutScreen from "./components/AboutScreen";
 import ProfileScreen from './components/ProfileScreen';
 import ResetPasswordScreen from './components/auth/ResetPasswordScreen';
 import ForgotPasswordScreen from './components/auth/ForgotPasswordScreen';
+import SplashScreen from './components/SplashScreen';
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
+
+  state = {
+    userToken: null,
+    isLoading: true
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <SplashScreen />;
+    }
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          { true ? (
+          {!this.state.userToken ? (
             <>
-              <Stack.Screen name="SignUp" component={SignUpScreen} />
               <Stack.Screen name="SignIn" component={SignInScreen} />
+              <Stack.Screen name="SignUp" component={SignUpScreen} />
               <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
               <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
             </>
@@ -28,7 +38,7 @@ export default class App extends Component {
               <>
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="About" component={AboutScreen} />
-                <Stack.Screen name="Profile" component={ProfileScreen}/>
+                <Stack.Screen name="Profile" component={ProfileScreen} />
               </>
             )}
         </Stack.Navigator>
@@ -37,11 +47,3 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
